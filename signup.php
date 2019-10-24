9741eb0b79f84a24f42beadc79d34de533a3012c
OarabileMwiya <42150074+OarabileMwiya@users.noreply.github.com>
2019-10-24 22:55:40 +0200

    Add files via upload
    Sign up template

---
+++ signup.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="css/signupcss.css">
+  </head>
+  <body>
+
+	<div class="login-box">
+  <h1>SignUp</h1>
+  <div class="textbox">
+    <i class="fas fa-user"></i>
+
+    <form action="registration.php" method="post">
+    <input type="text" name="first" placeholder="Firstname" required>
+   </div>
+
+   <div class="textbox">
+   		<i class="fas fa-user"></i>
+   			
+    		<input type="text" name="last" placeholder="Lastname" required>
+   	</div>
+
+   	 <div class="textbox">
+   		<i class="fas fa-user"></i>
+   			
+    		<input type="text" name="user" placeholder="Username" required>
+   	</div>
+	<div class="textbox">
+    <i class="fas fa-lock"></i>
+    <input type="password" name="password" placeholder="Password" required>
+  	</div>
+
+  	<button type="submit" class="btn" >SignIn</button></form>
+
+</div>
+
+		
+</section>
+
+</body>
+</html>
+
+
+
